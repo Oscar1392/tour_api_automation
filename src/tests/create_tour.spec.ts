c39577d791a3f73b1f552e3e9b768949ef75729e
askarduishenaliev <d.askar92@gmail.com>
2025-11-09 15:44:35 -0500

    add create tour test and config updates

---
+++ src/tests/create_tour.spec.ts
@@ -0,0 +1,45 @@
+import { getUser, signUp } from '../../helper/user';
+
+let cookies:string;
+describe('TOUR', () => {
+    it('Create tour', async () => {
+        // Test implementation for creating a new tour
+        const userImport = getUser('admin');
+        console.log(userImport, "userImport");
+        await signUp(userImport).then(res => {
+            expect(res.status).toBe(201);
+            expect(res.body.data.user.email).toBe(userImport.email.toLowerCase());
+            cookies = res.headers['set-cookie'];
+        })
+    await request
+    .post('/tours')
+    .set('Cookie', cookies)
+    .send({
+        "name": "TourForn6871",
+        "duration": 10,
+        "description": "Could be",
+        "maxGroupSize": 10,
+        "summary": "Test tour",
+        "difficulty": "easy",
+        "price": 100,
+        "rating": 4.8,
+        "imageCover": "tour-3-cover.jpg",
+        "ratingsAverage": 4.9,
+        "guides": [],
+        "startDates": ["2024-04-04"],
+        "startLocation": {
+            "type": "Point",
+            "coordinates": [-74.005974, 40.712776]
+        },
+        "locations": {
+            "type": "Point",
+            "coordinates": [-74.005974, 40.712776]
+        }
+    })
+    .then(res => {
+        console.log(res.body, "create tour response");
+        expect(res.status).toBe(201);
+        expect(res.body.data.name).toBe("TourForn6871");
+    });
+    });
+});
